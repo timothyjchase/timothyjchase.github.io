44149bb131c3905dbcb48f07e919cd70a2216b7d
Timothy Chase <timothychase@Timothys-MacBook-Pro.local>
2018-10-22 20:46:21 -0400

    New build

---
+++ claustrophobia/precache-manifest.4feee883a84c956b73ecdac5ba5e12af.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e8f99eaaa727701a6db",
+    "url": "/claustrophobia/static/js/runtime~main.8e8f99ea.js"
+  },
+  {
+    "revision": "655e33aec32af699ae21",
+    "url": "/claustrophobia/static/js/main.655e33ae.chunk.js"
+  },
+  {
+    "revision": "9eeb27b0661d66a63568",
+    "url": "/claustrophobia/static/js/1.9eeb27b0.chunk.js"
+  },
+  {
+    "revision": "23253826ecd94fd0cceaa906c5ba1027",
+    "url": "/claustrophobia/index.html"
+  }
+];
--- claustrophobia/service-worker.js
+++ claustrophobia/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.937f6ff55b6813b2433e3c4ce8b5fd0f.js"
+  "/claustrophobia/precache-manifest.4feee883a84c956b73ecdac5ba5e12af.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/claustrophobia/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
