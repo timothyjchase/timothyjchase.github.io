bdff19e2417d199aed6a1038ce3abc5b0a681e2e
Timothy Chase <timothychase@Timothys-MacBook-Pro.local>
2018-10-29 12:18:36 -0400

    New build

--- claustrophobia/service-worker.js
+++ claustrophobia/service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/claustrophobia/precache-manifest.72126565b6d07e3b71995e65da1460a2.js"
+  "/claustrophobia/precache-manifest.1a0d878bd33cb864f3d47bc676fc2d42.js"
 );
 
 workbox.clientsClaim();
